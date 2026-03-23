9efefe2b41030b09ed25d473fa5487cae1a71a3c
Neikkie <neikkie21@outlook.com>
2026-03-22 21:13:16 -0400

    Initial Commit

---
+++ Health Track Daily Wellness/Health_Track_Daily_WellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_Track_Daily_WellnessApp.swift
+//  Health Track Daily Wellness
+//
+//  Created by Shanique Beckford on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Health_Track_Daily_WellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
